69d3a2c918266eff58f87560e3864d8532bc9010
Roman <vojtus.roman@gmail.com>
2023-01-01 20:38:18 +0100

    .

--- Open-Lab-05.04/StringTools.cs
+++ Open-Lab-05.04/StringTools.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace Open_Lab_05._04
 {
@@ -6,7 +8,15 @@ namespace Open_Lab_05._04
     {
         public string RemoveSpecialCharacters(string original)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in original)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_' || c == '-' || c == ' ')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
         }
     }
 }
